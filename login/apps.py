af7dd0d74480c6cf7df2d878b1f41b8742950d47
linuxx456 <enriqueroman457@gmail.com>
2024-11-04 02:21:09 -0300

    enrique

--- login/apps.py
+++ login/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 class LoginConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'login'  # Asegúrate de que esté en minúsculas
+    name = 'login'
--- login/urls.py
+++ login/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login_register_view, name='login'),  # Login y registro
-    path('home/', views.home, name='home'),  # Página de inicio
+    path('', views.login_register_view, name='login'), 
+    path('home/', views.home, name='home'), 
 ]
