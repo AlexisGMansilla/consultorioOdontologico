2e20b80ede0dcfeff93f5583013498c32ce0d1ff
AlexisGMansilla <alexisgmansilla@gmail.com>
2024-11-04 03:25:15 -0300

    front

--- pacientes/forms.py
+++ pacientes/forms.py
@@ -7,5 +7,10 @@ class PacienteForm(forms.ModelForm):
         model = Paciente
         fields = ['nombre', 'apellido', 'dni', 'fecha_nacimiento', 'telefono', 'direccion', 'genero', 'email', 'observacion']
         widgets = {
-            'fecha_nacimiento': forms.DateInput(attrs={'type': 'date'}),
+            'fecha_nacimiento': forms.DateInput(attrs={'type': 'date'}, format='%Y-%m-%d'),
         }
+# Override para asegurar el formato correcto
+    def __init__(self, *args, **kwargs):
+        super(PacienteForm, self).__init__(*args, **kwargs)
+        if self.instance and self.instance.fecha_nacimiento:
+            self.fields['fecha_nacimiento'].initial = self.instance.fecha_nacimiento.strftime('%Y-%m-%d')
--- pacientes/views.py
+++ pacientes/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Paciente  # Asegúrate de tener el modelo Paciente o ajusta el nombre según tu modelo
 from django.contrib.auth.decorators import login_required
 from .forms import PacienteForm
+from django.contrib import messages
 
 @login_required
 def pacientes_view(request):
@@ -10,7 +11,19 @@ def pacientes_view(request):
 
 def historia_clinica(request, paciente_id):
     paciente = get_object_or_404(Paciente, id=paciente_id)
-    return render(request, 'pacientes/historia_clinica.html', {'paciente': paciente})
+    
+    if request.method == 'POST':
+        form = PacienteForm(request.POST, instance=paciente)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Los datos del paciente han sido actualizados.')
+            return redirect('pacientes')  # Redirige a la vista de pacientes después de guardar
+        else:
+            messages.error(request, 'Hubo un error al actualizar los datos.')
+    else:
+        form = PacienteForm(instance=paciente)
+    
+    return render(request, 'pacientes/historia_clinica.html', {'form': form, 'paciente': paciente})
 
 def crear_paciente(request):
     if request.method == 'POST':
