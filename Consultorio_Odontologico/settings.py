1b644c30c4d36cac3d2f0d4b8a8f2f4bd51a7369
AlexisGMansilla <alexisgmansilla@gmail.com>
2024-11-04 02:19:59 -0300

    Add changes

--- Consultorio_Odontologico/settings.py
+++ Consultorio_Odontologico/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'login',
+    'pacientes',
 ]
 
 MIDDLEWARE = [
@@ -81,7 +82,7 @@ DATABASES = {
     }
 }
 
-
+LOGOUT_REDIRECT_URL = 'login'
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
@@ -116,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- login/urls.py
+++ login/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('', views.login_register_view, name='login'),  # Login y registro
     path('home/', views.home, name='home'),  # Página de inicio
+    path('logout/', LogoutView.as_view(), name='logout'),  # URL para cerrar sesión
 ]
