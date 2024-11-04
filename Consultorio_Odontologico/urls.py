bd2a8ae002bc2b38c71df080c38d9920592a1b80
AlexisGMansilla <alexisgmansilla@gmail.com>
2024-11-04 02:53:27 -0300

    fix logout

--- Consultorio_Odontologico/urls.py
+++ Consultorio_Odontologico/urls.py
@@ -1,26 +1,10 @@
-"""
-URL configuration for Consultorio_Odontologico project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('login/', include('login.urls')),  # Asegúrate de que está bien escrito
-    path('pacientes/', include('pacientes.urls')),
-    path('', RedirectView.as_view(url='/login/', permanent=True)),  # Redirección a la página de login
+    path('admin/', admin.site.urls),  # URL para acceder al panel de administración
+    path('login/', include('login.urls')),  # Incluye las URLs de la app login
+    path('pacientes/', include('pacientes.urls')),  # Incluye las URLs de la app pacientes
+    path('', RedirectView.as_view(url='/login/', permanent=True)),  # Redirecciona la raíz del sitio a la página de login
 ]
