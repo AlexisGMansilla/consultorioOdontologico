3c82c8fc9b112f36aa5212a16fabc0c11bbc11dd
AlexisGMansilla <alexisgmansilla@gmail.com>
2024-11-28 04:00:27 -0300

    add inicio

--- Consultorio_Odontologico/views.py
+++ Consultorio_Odontologico/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from datetime import datetime
+from datetime import datetime, date
+from turnos.models import Turno  # Asegúrate de importar el modelo de turnos correctamente
 
 def home(request):
-    today = datetime.now()
+    today = date.today()  # Obtén la fecha actual
+    # Filtra los turnos de la fecha actual
+    turnos_hoy = Turno.objects.filter(fecha=today).order_by('hora')
+    
     return render(request, 'home.html', {
         'year': today.year,
         'month': today.month,
+        'turnos_hoy': turnos_hoy  # Pasa los turnos al template
     })
